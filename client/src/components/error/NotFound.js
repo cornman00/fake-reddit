6fe94d86b1281b457183716841054dce46ccc26f
Seokhyun Kim <seokhyunkim@Seokhyuns-MacBook-Pro.local>
2020-08-20 16:27:32 -0500

    Added not found page

---
+++ client/src/components/error/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <i class="fas fa-bug"></i>
+      <br />
+
+      <p id="nf-msg">Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
